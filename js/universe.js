ad356309d25654ca81c3e6b2dff97f5e4e53a13d
asa-world <1923793974@qq.com>
2022-12-30 19:45:07 +0800

    Site updated: 2022-12-30 19:45:06

--- js/universe.js
+++ js/universe.js
@@ -28,3 +28,25 @@ function show_date_time(){
     span_dt_dt.innerHTML=' <font style=color:#2d85f0>'+daysold+'</font> 天 <font style=color:#f4433c>'+hrsold+'</font> 时 <font style=color:#ffbc32>'+minsold+'</font> 分 <font style=color:#0aa858>'+seconds+'</font> 秒';
     }
 show_date_time();
+
+// 返回顶部 显示网页阅读进度
+window.onscroll = percent; // 执行函数
+// 页面百分比
+function percent() {
+  let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
+    b =
+      Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.offsetHeight,
+        document.body.clientHeight,
+        document.documentElement.clientHeight
+      ) - document.documentElement.clientHeight, // 整个网页高度 减去 可视高度
+    result = Math.round((a / b) * 100), // 计算百分比
+    btn = document.querySelector("#percent"); // 获取图标
+
+  result <= 99 || (result = 99), (btn.innerHTML = result);
+}
+
+document.getElementById("page-name").innerText = document.title.split(" | asa-world")[0];;
